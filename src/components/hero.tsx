dfdc1535db75bd7719e2dd1edfd94fc8eba0787a
SAKTHI-STARK <sakthi492003@gmail.com>
2025-05-17 08:29:29 +0530

    change the mobile vew of hero page

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,12 +5,13 @@ import { ComputersCanvas } from "./canvas";
 import { styles } from "../styles";
 import { cn } from "../utils/lib";
 
+
 // Animated phrases
 const phrases = [
   "AI Model Trainer",
   "Full Stack Web Developer",
   "Python Developer",
-  "Data Analyst"
+  "Data Analyst",
 ];
 
 export const Hero = () => {
@@ -24,15 +25,14 @@ export const Hero = () => {
 
     checkScreenSize();
     window.addEventListener("resize", checkScreenSize);
-
     return () => window.removeEventListener("resize", checkScreenSize);
   }, []);
 
-  // Loop through phrases
+  // Rotate phrases
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prev) => (prev + 1) % phrases.length);
-    }, 2500); // every 2.5 seconds
+    }, 2500);
     return () => clearInterval(interval);
   }, []);
 
@@ -44,19 +44,17 @@ export const Hero = () => {
           "absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5"
         )}
       >
-        {/* Left Side Bar */}
+        {/* Left Bar */}
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff] animate-fade-in" />
           <AnimatedBar />
         </div>
 
-        {/* Right Content */}
+        {/* Text Content */}
         <div>
           <h1 className={cn(styles.heroHeadText, "text-white animate-fade-in")}>
             Hi, I'm <span className="text-[#915eff]">Sakthivel</span>
           </h1>
-
-          {/* Animated Text */}
           <div className="h-[60px] mt-2 sm:pb-4 overflow-hidden relative">
             <AnimatePresence mode="wait">
               <motion.p
@@ -74,8 +72,19 @@ export const Hero = () => {
         </div>
       </div>
 
-      {/* 3D Model */}
+      {/* 3D Model (Desktop Only) */}
       {!isMobile && <ComputersCanvas />}
+
+      {/* Mobile Image */}
+      {isMobile && (
+          <div className="absolute inset-0 flex justify-center items-end pb-10">
+    <img
+      src="src/assets/desktop_img.png"
+      alt="Mobile Illustration"
+      className="w-[100%] h-auto max-h-[100%] object-contain mb-20"
+    />
+  </div>
+      )}
     </section>
   );
 };
