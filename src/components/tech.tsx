96e8bdd97ff88fab5cb832306e0ac8f155b6463f
SAKTHI-STARK <sakthi492003@gmail.com>
2025-05-17 14:41:44 +0530

    change tech stack in mobile view

--- src/components/tech.tsx
+++ src/components/tech.tsx
@@ -2,17 +2,24 @@ import { BallCanvas } from "./canvas";
 import { TECHNOLOGIES } from "../constants";
 import { SectionWrapper } from "../hoc";
 
-// Technologies
+const excludeTechNames = ["Three JS", "Tailwind CSS"];
+
 export const Tech = () => {
   return (
     <SectionWrapper>
-      <div className="flex flex-row flex-wrap justify-center gap-10">
-        {/* Iterate over each technology */}
-        {TECHNOLOGIES.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
-          </div>
-        ))}
+      <div className="w-full flex flex-row flex-wrap justify-center gap-10">
+        {TECHNOLOGIES.map((technology) => {
+          // For the two excluded techs, add hidden on mobile classes
+          const isExcluded = excludeTechNames.includes(technology.name);
+          return (
+            <div
+              className={`w-28 h-28 ${isExcluded ? "hidden sm:block" : ""}`}
+              key={technology.name}
+            >
+              <BallCanvas icon={technology.icon} />
+            </div>
+          );
+        })}
       </div>
     </SectionWrapper>
   );
